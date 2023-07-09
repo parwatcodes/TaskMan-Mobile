5e371e4d115a99732435d1e0ae67792ea8cf65e3
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-07-09 17:53:10 -0400

    enhance: login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,60 +1,72 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image, Pressable, ImageBackground } from 'react-native';
 
 import { LOGO_URL } from '../constants';
-import { borderColor, backgroundColor, white } from '../constants/colors';
+import { borderColor, backgroundColor, white, lightBlue } from '../constants/colors';
 
 const Login = (props) => {
   const { email, password, handleLogin } = props;
 
   return (
     <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <Image
-          style={styles.logo}
-          source={{
-            uri: LOGO_URL
-          }}
-        />
-        <Text style={styles.text}>Sign in to TaskMan</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          value={email}
-        // onChangeText={text => setEmail(text)}
-        />
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry
-          value={password}
-        // onChangeText={text => setPassword(text)}
-        />
-        <TouchableOpacity style={styles.button}>
-          <Button color={white} title="Login" onPress={() => {
-          console.log('cc')
-          handleLogin()
-        }} />
-        </TouchableOpacity >
-      </View>
+      <ImageBackground source={{
+        uri: LOGO_URL
+      }} style={{
+        flex: 1
+      }}>
+        <View style={styles.contentWrapper}>
+          <View style={styles.topContainer}>
+            <Image
+              style={styles.logo}
+              source={{
+                uri: LOGO_URL
+              }}
+            />
+            <Text style={styles.text}>Sign in to TaskMan</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput
+              style={styles.input}
+              value={email}
+            // onChangeText={text => setEmail(text)}
+            />
+            <Text style={styles.label}>Password</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              value={password}
+            // onChangeText={text => setPassword(text)}
+            />
+            <Pressable onPress={() => {
+              console.log('cc');
+              handleLogin();
+            }}>
+              <Text style={styles.button} >Login</Text>
+            </Pressable >
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    margin: '10%',
-    borderColor: 'gray',
+    height: '100%',
+    width: '100%',
+    // backgroundColor: lightBlue
+  },
+  contentWrapper: {
+    margin: '10%'
   },
   topContainer: {
     marginBottom: 30,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   logo: {
-    width: 70,
-    height: 70,
+    width: 80,
+    height: 80,
     resizeMode: "contain",
     marginBottom: 5
   },
@@ -62,13 +74,12 @@ const styles = StyleSheet.create({
     backgroundColor,
     padding: 20,
     borderWidth: 1,
-    borderRadius: 10,
+    borderRadius: 5,
     borderColor
-
   },
   input: {
     width: '100%',
-    height: 40,
+    height: 45,
     borderWidth: 1,
     borderRadius: 5,
     borderColor,
@@ -77,14 +88,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   text: {
-    fontSize: 22,
+    fontSize: 24,
     fontWeight: 500
   },
   button: {
     backgroundColor: 'green',
     width: '100%',
     borderRadius: 5,
-    marginTop: 20
+    marginTop: 20,
+    overflow: 'hidden',
+    color: white,
+    textAlign: 'center',
+    paddingVertical: 10,
+    fontSize: 18,
+    fontWeight: 600
   },
   label: {
     fontSize: 14,
