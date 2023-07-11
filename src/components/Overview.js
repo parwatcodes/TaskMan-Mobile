f08f52a7f44f20095dc4b38eee0010d51e142c03
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-07-11 18:54:22 -0400

    Add Image background in overview screen

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ImageBackground } from 'react-native';
 
-import { statusToCardColor } from '../helpers/mappings';
+import { statusToCardColor, TASK_STATUS } from '../helpers/mappings';
 import { goldTips, portage, white } from '../helpers/colors';
+import { LOGO_URL } from '../helpers/constant';
 
 const renderItem = ({ item }) => (
   <View style={styles.itemContainer}>
     <View style={{ ...styles.item, backgroundColor: statusToCardColor[item.status || 'default'] }}>
-      <Text style={styles.title}>{item.total} {item.status}</Text>
-      <Text>{item.type}</Text>
+      <Text style={styles.title}>{TASK_STATUS[item.status]}</Text>
+      <View style={{
+        alignItems: 'baseline',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginLeft: 10
+      }}>
+        <Text style={{
+          fontSize: 40,
+          fontWeight: 600,
+          textAlign: 'center',
+          color: 'rgba(255, 255, 255, 0.85)',
+          textShadowColor: 'rgba(0, 0, 0, 0.75)',
+          textShadowOffset: { width: 1, height: 1 },
+          textShadowRadius: 1
+        }}>{item.total}</Text>
+        <Text style={{
+          fontSize: 10,
+          fontWeight: 600
+        }}>
+          &nbsp;
+          {item.type}(s)</Text>
+      </View>
     </View>
   </View>
 );
@@ -17,25 +39,41 @@ const Overview = (props) => {
   let { data } = props;
 
   return (
-    <View style={styles.container}>
-    <FlatList
-      data={data}
-      numColumns={2}
-      renderItem={renderItem}
-      keyExtractor={(item, index) => index.toString()}
-    />
-    <View style={styles.reportContainer}>
-      <Text>Task Analysis</Text>
-    </View>
-
+    <View style={styles.main}>
+        <ImageBackground source={{
+          uri: LOGO_URL
+        }} style={{
+          flex: 1
+        }}>
+      <View style={styles.container}>
+          <View>
+            <FlatList
+              data={data}
+              numColumns={2}
+              renderItem={renderItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+          <View style={styles.reportContainer}>
+            <View>
+              <Text>Report Analysis</Text>
+            </View>
+          </View>
+      </View >
+        </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  main: {
+    height: "100%",
+    width: "100%"
+  },
   container: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
+    margin: 10,
+    flexDirection: 'column',
+    flex: 1
   },
   itemContainer: {
     flex: 1,
@@ -56,10 +94,9 @@ const styles = StyleSheet.create({
     fontWeight: 500
   },
   reportContainer: {
+    flex: 1,
     backgroundColor: goldTips,
-    height: '50%',
     marginTop: 10,
-    marginBottom: 10,
     padding: 16,
     borderRadius: 8,
     shadowColor: '#000',
